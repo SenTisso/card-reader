122484ec9fc03e647ab1a2bd42c5efac222345ac
Jiří Hulmák <jirka.hulmak@gmail.com>
2021-01-16 21:51:27 +0100

    odstrani e-blbustky z CardListViewModel

--- card-reader/card-reader/ViewModels/CardListViewModel.cs
+++ card-reader/card-reader/ViewModels/CardListViewModel.cs
@@ -8,14 +8,9 @@ namespace card_reader.ViewModels
 {
     public class CardListViewModel : BaseViewModel
     {
-        // TODO: takto se nastavuji custom properties u samotnych ViewModelu
-        public string Roman { get; }
-
-
         public CardListViewModel()
         {
             Title = "Big PP CardList";
-            Roman = "roman";
         }
     }
 }
