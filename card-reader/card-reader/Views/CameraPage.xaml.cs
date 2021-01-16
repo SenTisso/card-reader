5def041fff33523135ca0b8f2800a62be1ee6fe6
Jiří Hulmák <jirka.hulmak@gmail.com>
2021-01-16 13:02:24 +0100

    prida todo a odstrani prebytecny zakomentovany kod

--- card-reader/card-reader/Views/CameraPage.xaml.cs
+++ card-reader/card-reader/Views/CameraPage.xaml.cs
@@ -40,52 +40,6 @@ namespace card_reader.Views
                     Navigation.PushAsync(new CardDetailPage(new CardDetailViewModel(SavedCard)));
                 });
             }
-
-
-            // MessagingCenter.Send<CameraPage>(this, "Hi");
-
-            // var bitmapMatrix = new MultiFormatWriter().encode(result.Text, result.BarcodeFormat, 800, 400);
-            //
-            // var width = bitmapMatrix.Width;
-            // var height = bitmapMatrix.Height;
-            // int[] pixelsImage = new int[width * height];
-            // Bitmap bitmap = new Bitmap(800, 400, PixelFormat.Format32bppArgb);
-            //
-            // for (int i = 0; i < height; i++)
-            // {
-            //     for (int j = 0; j < width; j++)
-            //     {
-            //         if (bitmapMatrix[j, i])
-            //             bitmap.SetPixel(i, j, Color.Black);
-            //         else
-            //             bitmap.SetPixel(i, j, Color.White);
-            //
-            //     }
-            // }
-            //
-            // bitmap.Save(@"C:\code\test.png", ImageFormat.Png);
-
-            // ZXingBarcodeImageView barcode = new ZXingBarcodeImageView() {
-            //     BarcodeOptions =
-            //     {
-            //         Width = 800,
-            //         Height = 400
-            //     },
-            //     BarcodeFormat = result.BarcodeFormat,
-            //     BarcodeValue = result.Text
-            // };
-
-            // Bitmap bitmap = Bitmap.CreateBitmap(width, height, Bitmap.Config.Argb8888);
-            // bitmap.SetPixels(pixelsImage, 0, width, 0, 0, width, height);
-            //
-            // var stream = new FileStream(path, FileMode.Create);
-            // bitmap.Compress(Bitmap.CompressFormat.Jpeg, 100, stream);
-            // stream.Close();
-
-            // Bitmap bitmap = writer.Write("");
-            // Bitmap bitmap = writer.Write("");
-            // bitmap.Save(HttpContext.Response.Body,System.Drawing.Imaging.ImageFormat.Png);
-            // return; // there's no need to return a `FileContentResult` by `File(...);`
         }
     }
 }
