e9b7098acce2c223bf7f41c0afce394c75a54801
Jiří Hulmák <jirka.hulmak@gmail.com>
2021-01-17 19:37:04 +0100

    opravi predavani barvy CameraPage, tudiz ukladani spravne barvy

--- card-reader/card-reader/Views/ChooseCardPage.xaml.cs
+++ card-reader/card-reader/Views/ChooseCardPage.xaml.cs
@@ -14,6 +14,7 @@ namespace card_reader.Views
     public partial class ChooseCardPage : ContentPage
     {
         static Random rnd = new Random();
+        private string CardColor;
         private new List<string> Colors = new List<String>() { "#ffcdd2", "#f8bbd0", "#e1bee7", "#d1c4e9", "#c5cae9", "#bbdefb", "#b3e5fc", "#b2ebf2", "#b2dfdb", "#c8e6c9", "#dcedc8", "#f0f4c3", "#fff9c4", "#ffecb3", "#ffe0b2", "#ffccbc", "#d7ccc8", "#cfd8dc" };
 
         public ChooseCardPage()
@@ -30,7 +31,7 @@ namespace card_reader.Views
         }*/
         async void OnNextClicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new CameraPage (cardName.Text, boxColor.Color.ToString()));
+            await Navigation.PushAsync(new CameraPage (cardName.Text, this.CardColor));
         }
 
         async void OnTextChanged(object sender, EventArgs e)
@@ -44,7 +45,8 @@ namespace card_reader.Views
             }
             
             int r = rnd.Next(Colors.Count);
-            boxColor.Color = Color.FromHex(Colors[r]);
+            this.CardColor = Colors[r];
+            boxColor.Color = Color.FromHex(this.CardColor);
         }
     }
 }
